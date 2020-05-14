9e017a6f22459897d40ae10f0757af2c798dee57
meeshcompbio <jmmichno2@gmail.com>
2020-05-14 09:54:36 -0500

    setup and initial doc for teaching intro to stat learning

---
+++ Intro_To_Stat_Learning/README.md
@@ -0,0 +1,15 @@
+# Introduction to statistical learning
+
+Using this course to teach intro stats modified for use with python instead of R (<https://online.stanford.edu/courses/sohs-ystatslearning-statistical-learning)>
+
+## Installation
+
+Using a conda environment
+
+```bash
+conda create -n IntroStatLearning python=3.8
+conda activate IntroStatLearning
+pip install -r install_requires.txt
+pip install -r requirements.txt
+bash grab_datasets.sh
+```
---
+++ Intro_To_Stat_Learning/grab_datasets.sh
@@ -0,0 +1,9 @@
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Advertising.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Auto.data -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Auto.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/College.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Ch10Ex11.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Credit.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Income1.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Income2.csv -P Data
+wget --span-hosts http://faculty.marshall.usc.edu/gareth-james/ISL/Heart.csv -P Data
